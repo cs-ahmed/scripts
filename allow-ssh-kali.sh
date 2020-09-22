42b7deb3800cad3cf57d6bc2b06c31af41614e85
Ahmed Ibrahim <aibrahim@pitt.edu>
2020-09-22 18:22:45 -0400

    Allow SSH Root Login on Kali 2018

---
+++ allow-ssh-kali.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo ""
+echo "ENABLE ssh..."
+echo ""
+sudo systemctl start ssh
+
+echo ""
+echo ""
+echo "ALLOWING ROOT SSH ..."
+sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+sudo service ssh restart
+echo ""
+echo ""
+echo "ALLOWED!"
+
+echo ""
+echo ""
+echo "DON'T FORGET cd .."
+echo "DON'T FORGET rm -R scripts/"
+
+echo ""
+echo ""
+echo "DON'T FORGET rm ./.bash_history"
+echo "Then, reboot  if needed"
